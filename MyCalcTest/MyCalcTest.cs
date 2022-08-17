c2e076602ae1451a4a878bd4d37fbd157fe3e1f7
Amma Ashok <E009117@cigniti.com>
2022-08-17 17:51:52 +0530

    newupdate

--- MyCalcTest/MyCalcTest.cs
+++ MyCalcTest/MyCalcTest.cs
@@ -71,7 +71,7 @@ namespace Lab_Excer
         public void TestDataDivMethod(double op1,double op2,double expresult)
         {
             double actualresult=MyCalc.Div(op1,op2);
-            Assert.AreEqual(expresult,actualresult,"Mul test case is failed");
+            Assert.AreEqual(expresult,actualresult,"Div test case is failed");
         }
         [DataTestMethod]
         [DataRow (20,10,5)]
@@ -81,17 +81,27 @@ namespace Lab_Excer
         public void NegativeTestDataDivMethod(double op1,double op2,double expresult)
         {
             double actualresult=MyCalc.Div(op1,op2);
-            Assert.AreNotEqual(expresult,actualresult,"Mul test case is failed");
+            Assert.AreNotEqual(expresult,actualresult,"Div test case is failed");
+        }
+        [DataTestMethod]
+        [DataRow (2,2,4)]
+        [DataRow (3,3,27)]
+        [DataRow (4,3,64)]
+
+        public void TestDataPowMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Pow(op1,op2);
+            Assert.AreEqual(expresult,actualresult,"Pow test case is failed");
         }
         [DataTestMethod]
         [DataRow (20,10,5)]
         [DataRow (18,10,1)]
         [DataRow (28,17,5)]
 
-        public void TestDataPowMethod(double op1,double op2,double expresult)
+        public void NegativeTestDataPowMethod(double op1,double op2,double expresult)
         {
             double actualresult=MyCalc.Pow(op1,op2);
-            Assert.AreEqual(expresult,actualresult,"Mul test case is failed");
+            Assert.AreNotEqual(expresult,actualresult,"Pow test case is failed");
         }
         
         
