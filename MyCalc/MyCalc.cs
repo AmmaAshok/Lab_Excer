3ad87128e631b7b7e31f48821a7fe58050f749d3
Amma Ashok <E009117@cigniti.com>
2022-08-17 17:28:11 +0530

    update

---
+++ MyCalc/MyCalc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lab_Excer
+{
+    public class MyCalc
+    {
+        public static double Add(double op1,double op2)
+
+            {
+                return op1+op2;
+            }
+       public static double Sub(double op1,double op2)
+
+            {
+                return op1-op2;
+            }
+
+            
+    public static double Div(double op1,double op2)
+
+            {
+                return op1/op2;
+            }
+    public static double Mul(double op1,double op2)
+
+            {
+                return op1*op2;
+            }
+    
+    public static double Pow(double op1,double op2)
+
+               
+            {
+               double Pow=Math.Pow(op1,op2);
+              return Pow;     
+            } 
+
+
+            
+     
+     public static int Power(int p1,int p2)
+    {
+        int p3=1;
+        for (int i=p1; i<=p2;i++)
+        { 
+            p3=p3*p1;
+        }
+        return p3;
+    }
+
+          }
+}
---
+++ MyCalcTest/MyCalcTest.cs
@@ -0,0 +1,100 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace Lab_Excer
+{
+    [TestClass]
+    public class MyCalcTest
+    {
+        
+        
+        [DataTestMethod]
+        [DataRow (20,10,30)]
+        [DataRow (20,18,38)]
+        [DataRow (32,16,48)]
+        public void TestDataAddMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Add(op1,op2);
+            Assert.AreEqual(expresult,actualresult,"Add test case is failed");
+        }
+        [DataTestMethod]
+        [DataRow (20,10,12)]
+        [DataRow (20,18,28)]
+        [DataRow (32,16,38)]
+        public void NegativeTestDataAddMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Add(op1,op2);
+            Assert.AreNotEqual(expresult,actualresult,"Add test case is failed");
+        }
+       [DataTestMethod]
+        [DataRow (20,10,10)]
+        [DataRow (26,10,16)]
+        [DataRow (28,08,20)]
+        public void TestDataSubMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Sub(op1,op2);
+            Assert.AreEqual(expresult,actualresult,"Add test case is failed");
+        }
+        [DataTestMethod]
+        [DataRow (20,10,05)]
+        [DataRow (18,12,12)]
+        [DataRow (38,12,16)]
+        public void NegativeTestDataSubMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Sub(op1,op2);
+            Assert.AreNotEqual(expresult,actualresult,"Sub test case is failed");
+        }
+        [DataTestMethod]
+        [DataRow (20,10,200)]
+        [DataRow (18,10,180)]
+        [DataRow (20,12,240)]
+        public void TestDataMulMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Mul(op1,op2);
+            Assert.AreEqual(expresult,actualresult,"Mul test case is failed");
+        }
+       [DataTestMethod]
+        [DataRow (20,10,20)]
+        [DataRow (18,10,18)]
+        [DataRow (20,10,10)]
+        public void NegativeTestDataMulMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Mul(op1,op2);
+            Assert.AreNotEqual(expresult,actualresult,"Mul test case is failed");
+        }
+        [DataTestMethod]
+        [DataRow (20,10,2)]
+        [DataRow (18,10,1.8)]
+        [DataRow (28,7,4)]
+
+        public void TestDataDivMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Div(op1,op2);
+            Assert.AreEqual(expresult,actualresult,"Mul test case is failed");
+        }
+        [DataTestMethod]
+        [DataRow (20,10,5)]
+        [DataRow (18,10,1)]
+        [DataRow (28,17,5)]
+
+        public void NegativeTestDataDivMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Div(op1,op2);
+            Assert.AreNotEqual(expresult,actualresult,"Mul test case is failed");
+        }
+        [DataTestMethod]
+        [DataRow (20,10,5)]
+        [DataRow (18,10,1)]
+        [DataRow (28,17,5)]
+
+        public void TestDataPowMethod(double op1,double op2,double expresult)
+        {
+            double actualresult=MyCalc.Pow(op1,op2);
+            Assert.AreEqual(expresult,actualresult,"Mul test case is failed");
+        }
+        
+        
+        
+    }
+}
